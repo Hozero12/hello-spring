4b5cb0577bc20310d3c8cd8b76f99fef98234332
packa <amb258012@gmail.com>
2020-08-12 23:01:36 +0900

    jdbc템플릿을 사용해서 데이터베이스와 연동

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -10,11 +10,11 @@ import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
     private final MemberRepository memberRepository ; //=new MemoryMemberRepository();
 
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
